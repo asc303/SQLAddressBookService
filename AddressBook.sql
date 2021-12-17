a4cd1b42849ebd7f3cbee7c432e3f084bf7c3d89
asc303 <chauhansam94@gmail.com>
2021-12-17 23:04:48 +0530

    [ashutosh] add:Inserting Data

--- AddressBook.sql
+++ AddressBook.sql
@@ -5,3 +5,11 @@ create Table address_Book(id int identity (1,1) primary key, Firstname varchar(2
 exec sp_help address_Book
 select * from address_Book
 
+insert into address_Book(Firstname,Lastname,address,City,State,Zip,PhoneNumber,Email) Values
+( 'Ashutosh', 'Singh', 'Ghauhsabad', 'Varanasi', 'Uttar Pardesh', '221002', '9696896592', 'chauhansam94@gmail.com' ),
+( 'Abhi', 'Singh', 'Ghauhsabad', 'noida', 'Uttar Pardesh', '221002', '7896968574', 'singhabhi94@gmail.com' ),
+( 'Parth', 'Singh', 'Ghauhsabad', 'delhi', 'Uttar Pardesh', '221002', '9584858569', 'singhparth94@gmail.com' ),
+( 'Sam', 'Singh', 'Ghauhsabad', 'goa', 'Uttar Pardesh', '221002', '9695455455', 'singhsam94@gmail.com' ),
+( 'Ashish', 'Singh', 'Ghauhsabad', 'dehradun', 'Uttar Pardesh', '221002', '9692685632', 'singhashish94@gmail.com' ),
+( 'Aditya', 'Singh', 'Ghauhsabad', 'lko', 'Uttar Pardesh', '221002', '9696895582', 'singhaditya94@gmail.com' );
+
