e661c480ac1945b81d446706a671c2dac727cdf0
asc303 <chauhansam94@gmail.com>
2021-12-17 23:01:04 +0530

    [ashutosh] add:CreateTable

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,3 +1,7 @@
 create database AddressBook_Service;
 use AddressBook_Service;
 
+create Table address_Book(id int identity (1,1) primary key, Firstname varchar(25), Lastname varchar (25), address varchar(25), City varchar (30), State varchar (25), Zip varchar (10), PhoneNumber varchar(15), Email varchar()); 
+exec sp_help address_Book
+select * from address_Book
+
